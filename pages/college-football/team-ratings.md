fe5b1a7c40710c0787cb9f055ba2eb9e12c2cb26
gallochris <christopher.joseph.gallo@gmail.com>
2024-10-05 08:11:26 -0400

    Friday data and updates
    Adjust week 0 games to not be week 1, update how to order conference standings, and add color palette to viz.

--- pages/college-football/team-ratings.md
+++ pages/college-football/team-ratings.md
@@ -100,4 +100,5 @@ group by all
     title="F+ vs FPI percentiles by team"
     subtitle="Last updated September 30"
     shape=emptyCircle
+    colorPalette={['#ffffcc', '#ffeda0', '#fed976', '#feb24c', '#fd8d3c', '#fc4e2a', '#e31a1c', '#bd0026', '#800026']}
 />
--- queries/conference_standings.sql
+++ queries/conference_standings.sql
@@ -1,17 +1,20 @@
-select 
-      team,
-      conf,
-      division,
-      conf_win,
-      conf_loss,
-      full_diff,
-      h_w,
-      h_l,
-      home_diff,
-      a_w,
-      a_l,
-      away_diff
+  select 
+    team,
+    conf,
+    division,
+    conf_win,
+    conf_loss,
+    cast(conf_win as float) / nullif((conf_win + conf_loss), 0) as conf_win_pct,
+    full_diff,
+    h_w,
+    h_l,
+    home_diff,
+    a_w,
+    a_l,
+    away_diff
   from cfb.conference_standings
   where conf like '${inputs.conf.value}'
   group by all
-  order by conf_win desc
+  order by 
+    conf_win_pct desc,
+    full_diff desc
