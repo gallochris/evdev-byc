76f2665cb1cd7e5f167929383fb8795c47f768d6
gallochris <christopher.joseph.gallo@gmail.com>
2025-02-10 14:14:06 -0500

    Use Torvik to compute TRAM
    Adjusts the hoopR script to use Torvik instead

--- data/college-basketball/tram.R
+++ data/college-basketball/tram.R
@@ -1,30 +1,14 @@
 # -----------------------------
 # Load the utilities
-# adjusts conference names
 source(here::here("data/college-basketball/utils.R"))
 
-# pull the teams for torvik to kenpom 
-team_matcher <- cbbdata::cbd_teams() |> 
-                dplyr::select(torvik_team, team = kp_team) |> 
-                # add west ga, mercyhurst - new teams in 2025
-                dplyr::add_row(torvik_team = "West Georgia",
-                               team = "West Georgia") |> 
-                dplyr::add_row(torvik_team = "Mercyhurst",
-                               team = "Mercyhurst")
-
-
-# grab the raw ratings from kenpom 
-raw_ratings <- hoopR::kp_efficiency(min_year = 2025, max_year = 2025) |> 
-  dplyr::select(team, raw_o, raw_d) |> 
-  dplyr::mutate(raw_em = raw_o - raw_d)
-
-# now grab the four factors from kenpom to compute tram 
-tram_data <- hoopR::kp_fourfactors(min_year = 2025, max_year = 2025) |> 
+# now grab the four factors from torvik to compute tram
+tram_data <- cbbdata::cbd_torvik_team_factors(year = 2025) |> 
   dplyr::mutate(
-    to_pct = off_to_pct / 100,
-    or_pct = off_or_pct / 100,
-    d_to_pct = def_to_pct / 100,
-    d_or_pct = def_or_pct / 100
+    to_pct = tov_rate / 100,
+    or_pct = oreb_rate / 100,
+    d_to_pct = def_tov_rate / 100,
+    d_or_pct = dreb_rate / 100
   ) |> 
   dplyr::mutate(
     off_svi = ((100 - (100 * to_pct)) + (or_pct * (0.561 * (100 - (100 * to_pct))))),
@@ -33,29 +17,16 @@ tram_data <- hoopR::kp_fourfactors(min_year = 2025, max_year = 2025) |>
   ) |> 
   dplyr::left_join(raw_ratings, by = c("team")) |> 
   dplyr::mutate(adj_em = adj_o - adj_d) |> 
-  dplyr::select(team, conf, tram, raw_em, adj_em,
+  dplyr::select(team, conf, tram, adj_em,
                 off_svi, def_svi, 
                 to_pct, or_pct, d_to_pct, d_or_pct,
-                raw_o, raw_d, 
-                adj_o, adj_d, off_e_fg_pct, off_ft_rate,
-                def_e_fg_pct, def_ft_rate) |>
-  # fix team names that don't match
-  dplyr::mutate(team = dplyr::case_match(team,
-                "McNeese" ~ "McNeese St.",
-                "IU Indy" ~ "IUPUI", 
-                "CSUN" ~ "Cal St. Northridge",
-                "Nicholls" ~ "Nicholls St.",
-                "Southeast Missouri" ~ "Southeast Missouri St.",
-                "Kansas City" ~ "UMKC", 
-                "SIUE" ~ "SIU Edwardsville",
-                "East Texas A&M" ~ "Texas A&M Commerce",
-                team ~ team)) |> 
-  dplyr::left_join(team_matcher, by = "team") |> 
+                adj_o, adj_d, off_e_fg_pct = efg, off_ft_rate = ftr,
+                def_e_fg_pct = def_efg, def_ft_rate = def_ftr) |>
+  # fix team names and conferences
   dplyr::mutate(conf = conf_name_update(conf),
                 conf = conf_name_lookup(conf)
   )
   
-
 
 # ----------------------------- Write to duckdb
 write_to_duckdb(tram_data, "tram_data")
