10940415e44b4f13b76e0695d955f2819b6b11c0
gallochris <christopher.joseph.gallo@gmail.com>
2025-02-14 07:27:08 -0500

    Fresh data

--- data/college-basketball/wab_logs.R
+++ data/college-basketball/wab_logs.R
@@ -15,7 +15,7 @@ barts <- ratings |>
     # define the bubble team - tune this over time
     barthag = .875,
     # this number goes down, wab should go up
-    adj_o = 115.55,
+    adj_o = 115.38,
     # this number goes up, wab should go up
     adj_d = 99.49
   ) |>
