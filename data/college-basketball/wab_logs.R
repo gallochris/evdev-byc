69cb242fd9d75d26763fd0d7b0f03a1319caee3f
gallochris <christopher.joseph.gallo@gmail.com>
2025-02-26 09:14:35 -0500

    Refresh WAB data

--- data/college-basketball/wab_logs.R
+++ data/college-basketball/wab_logs.R
@@ -17,7 +17,7 @@ barts <- ratings |>
     # this number goes down, wab should go up
     adj_o = 115.7,
     # this number goes up, wab should go up
-    adj_d = 99.49
+    adj_d = 99.42
   ) |>
   dplyr::mutate(
     oHome = adj_o * (1 + hcMultiplier),
