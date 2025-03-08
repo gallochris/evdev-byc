7f2470adfb9f50fe505749c8490d448379e31ed5
gallochris <christopher.joseph.gallo@gmail.com>
2025-03-08 08:41:16 -0500

    WAB update

--- data/college-basketball/wab_logs.R
+++ data/college-basketball/wab_logs.R
@@ -15,9 +15,9 @@ barts <- ratings |>
     # define the bubble team - tune this over time
     barthag = .875,
     # this number goes down, wab should go up
-    adj_o = 115.97,
+    adj_o = 115.91,
     # this number goes up, wab should go up
-    adj_d = 99.65
+    adj_d = 99.70
   ) |>
   dplyr::mutate(
     oHome = adj_o * (1 + hcMultiplier),
